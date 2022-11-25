fafc87c260974353cc03d3c248b8c007d2f8a54d
KONG41 <sokkongchan@gmal.com>
2022-11-25 17:41:26 +0700

    fix reCaptcha

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -3,12 +3,37 @@ import { FiPhoneCall, FiMail } from 'react-icons/fi'
 import { FiMap } from 'react-icons/fi'
 import { Titled } from 'react-titled'
 import ReCAPTCHA from "react-google-recaptcha";
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const messageRequired = "Field Required";
+const schema = yup.object({
+  name: yup.string().required(messageRequired),
+  email: yup.string().email("Email would be email@gmail.com").required(messageRequired),
+  subject: yup.string().required(messageRequired),
+  message: yup.string().required(messageRequired),
+
+})
+
 const Contact = () => {
   const { t } = useTranslation();
   const recaptcha_site_key = "6LfcZDIjAAAAAEjsm_I8nlo-u_D9L1bkgg4unTvg";
   const recaptcha_secret_key = "6LfcZDIjAAAAAALBsxqXhFc1W4czb3fCI2-0QA8H";
-  const recaptchaRef = useRef()
+  const captchaRef = useRef(null)
+
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema)
+  });
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const onError = (errors) => {
+    console.log(errors.email.message);
+  };
+
   return (
     <section className="contact-container">
       <Titled title={title => `Contact Us | ${title}`} />
@@ -57,19 +82,26 @@ const Contact = () => {
           <div className="form-text col-lg-6 col-md-6 col-sm-12">
             <span className="tag-title">{t('Feel free to say Hi~')}</span>
             <h1 className="title">{t('contact_us')}</h1>
-            <form className="form">
-              <input type="text" placeholder={`${t('Your Name')} *`} />
-              <input type="text" placeholder={`${t('Your Email')} *`} />
-              <input type="text" placeholder={`${t('Your Subject')} *`} />
-              <input type="text" placeholder={`${t('Your Message')} *`} />
+            <form onSubmit={handleSubmit(onSubmit, onError)}>
+              <div className="form">
+                <input {...register("name", { required: true })} type="text" placeholder={`${t('Your Name')} *`} />
+
+                <input {...register("email", { required: true })} type="text" placeholder={`${t('Your Email')} *`} />
+
+                <input {...register("subject", { required: true })} type="text" placeholder={`${t('Your Subject')} *`} />
+
+                <input {...register("message", { required: true })} type="text" placeholder={`${t('Your Message')} *`} />
+
+              </div>
+              <ReCAPTCHA
+                sitekey={recaptcha_site_key}
+                ref={captchaRef}
+              />
+              <div className="submit-button">
+                <input type="submit" className="sb-btn" value={t('Submit Now')} />
+              </div>
             </form>
-            <ReCAPTCHA
-              sitekey={recaptcha_site_key}
-              ref={recaptchaRef}
-            />
-            <div className="submit-button">
-              <input type="submit" className="sb-btn" value={t('Submit Now')} />
-            </div>
+
           </div>
           <div className="image-banner col-lg-6 col-md-6 col-sm-12">
             <img src={`${require('../assets/image/PSD_9_-scaled-pkc6v7a13euiimjy5crvz533smx50ul7pwl5iqcoxw.jpg')}`} />
