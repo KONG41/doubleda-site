7c0258e86e61d3146b30d034a2d2c5c9c368bd11
KONG41 <sokkongchan@gmal.com>
2023-01-04 13:25:19 +0700

    fix speed preloader on firfox

--- public/ecohosting-main/assets/js/main.js
+++ public/ecohosting-main/assets/js/main.js
@@ -1,40 +1,42 @@
-(function ($) {
-  "use strict"
+(function($) {
+  "use strict";
 
   /* 1. Proloder */
-  $(window).on('load', function () {
+  $(window).on("load", function() {
     var timeout = setTimeout(() => {
-      clearTimeout(timeout)
-      $('#preloader-active').delay(450).fadeOut('slow');
-      $('body').delay(450).css({
-        'overflow': 'visible'
+      clearTimeout(timeout);
+      $("#preloader-active").delay(450).fadeOut("slow");
+      $("body").delay(450).css({
+        overflow: "visible"
       });
-    },0)
+    }, 500);
   });
 
   /* 2. sticky And Scroll UP */
-  $(window).on('scroll', function () {
+  $(window).on("scroll", function() {
     var scroll = $(window).scrollTop();
     if (scroll < 400) {
       $(".header-sticky").removeClass("sticky-bar");
-      $('#back-top').fadeOut(500);
+      $("#back-top").fadeOut(500);
     } else {
       $(".header-sticky").addClass("sticky-bar");
-      $('#back-top').fadeIn(500);
+      $("#back-top").fadeIn(500);
     }
   });
 
   // Scroll Up
-  $('#back-top a').on("click", function () {
+  $("#back-top a").on("click", function() {
     console.log("log from back-top");
-    $('body,html').animate({
-      scrollTop: 0
-    }, 800);
+    $("body,html").animate(
+      {
+        scrollTop: 0
+      },
+      800
+    );
 
     return false;
   });
 
-  
   /* 3. slick Nav */
   // mobile_menu
   // var menu = $('ul#navigation').end();
@@ -49,13 +51,17 @@
   /* 4. MainSlider-1 */
   // h1-hero-active
   function mainSlider() {
-    var BasicSlider = $('.slider-active');
-    BasicSlider.on('init', function (e, slick) {
-      var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+    var BasicSlider = $(".slider-active");
+    BasicSlider.on("init", function(e, slick) {
+      var $firstAnimatingElements = $(".single-slider:first-child").find(
+        "[data-animation]"
+      );
       doAnimations($firstAnimatingElements);
     });
-    BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
-      var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+    BasicSlider.on("beforeChange", function(e, slick, currentSlide, nextSlide) {
+      var $animatingElements = $(
+        '.single-slider[data-slick-index="' + nextSlide + '"]'
+      ).find("[data-animation]");
       doAnimations($animatingElements);
     });
     BasicSlider.slick({
@@ -64,58 +70,62 @@
       dots: false,
       fade: true,
       arrows: false,
-      prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
-      nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
-      responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
+      prevArrow:
+        '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
+      nextArrow:
+        '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true
+          }
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+          }
         }
-      },
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false
-        }
-      }
       ]
     });
 
     function doAnimations(elements) {
-      var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-      elements.each(function () {
+      var animationEndEvents =
+        "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+      elements.each(function() {
         var $this = $(this);
-        var $animationDelay = $this.data('delay');
-        var $animationType = 'animated ' + $this.data('animation');
+        var $animationDelay = $this.data("delay");
+        var $animationType = "animated " + $this.data("animation");
         $this.css({
-          'animation-delay': $animationDelay,
-          '-webkit-animation-delay': $animationDelay
+          "animation-delay": $animationDelay,
+          "-webkit-animation-delay": $animationDelay
         });
-        $this.addClass($animationType).one(animationEndEvents, function () {
+        $this.addClass($animationType).one(animationEndEvents, function() {
           $this.removeClass($animationType);
         });
       });
     }
   }
   var timeout = setTimeout(() => {
-    clearTimeout(timeout)
-     mainSlider();
-  }, 1600)
+    clearTimeout(timeout);
+    mainSlider();
+  }, 1000);
 
   /* 4. Testimonial Active*/
-  var testimonial = $('.h1-testimonial-active');
+  var testimonial = $(".h1-testimonial-active");
   if (testimonial.length) {
     testimonial.slick({
       dots: false,
@@ -124,8 +134,10 @@
       autoplay: true,
       loop: true,
       arrows: true,
-      prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
-      nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
+      prevArrow:
+        '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
+      nextArrow:
+        '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
       slidesToShow: 1,
       slidesToScroll: 1,
       responsive: [
@@ -152,23 +164,21 @@
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
-            arrows: false,
+            arrows: false
           }
         }
       ]
     });
   }
 
-
-
   /* 6. Nice Selectorp  */
-  var nice_Select = $('select');
+  var nice_Select = $("select");
   if (nice_Select.length) {
     nice_Select.niceSelect();
   }
 
   // Brand Active
-  $('.brand-active').slick({
+  $(".brand-active").slick({
     dots: false,
     infinite: true,
     autoplay: true,
@@ -183,7 +193,7 @@
           slidesToShow: 4,
           slidesToScroll: 3,
           infinite: true,
-          dots: false,
+          dots: false
         }
       },
       {
@@ -192,7 +202,7 @@
           slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
-          dots: false,
+          dots: false
         }
       },
       {
@@ -208,7 +218,7 @@
           slidesToShow: 1,
           slidesToScroll: 1
         }
-      },
+      }
 
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
@@ -217,23 +227,24 @@
   });
 
   /* 7. data-background */
-  $("[data-background]").each(function () {
-    $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
+  $("[data-background]").each(function() {
+    $(this).css(
+      "background-image",
+      "url(" + $(this).attr("data-background") + ")"
+    );
   });
-
 
   /* 10. WOW active */
   new WOW().init();
 
-  // 11. ---- Mailchimp js --------//  
+  // 11. ---- Mailchimp js --------//
   function mailChimp() {
     // $('#mc_embed_signup').find('form').ajaxChimp();
   }
   mailChimp();
 
-
   // 12 Pop Up Img
-  var popUp = $('.single_gallery_part, .img-pop-up');
+  var popUp = $(".single_gallery_part, .img-pop-up");
   if (popUp.length) {
     // popUp.magnificPopup({
     //   type: 'image',
@@ -243,7 +254,7 @@
     // });
   }
   // 12 Pop Up Video
-  var popUp = $('.popup-video');
+  var popUp = $(".popup-video");
   if (popUp.length) {
     // popUp.magnificPopup({
     //   type: 'iframe'
@@ -251,30 +262,28 @@
   }
 
   /* 13. counterUp*/
-  $('.counter').counterUp({
+  $(".counter").counterUp({
     delay: 10,
     time: 3000
   });
 
   /* 14. Datepicker */
-  $('#datepicker1').datepicker();
+  $("#datepicker1").datepicker();
 
   // 15. Time Picker
-  $('#timepicker').timepicker();
+  $("#timepicker").timepicker();
 
   //16. Overlay
   $(".snake").snakeify({
     speed: 200
   });
 
-
   //17.  Progress barfiller
 
-  $('#bar1').barfiller();
-  $('#bar2').barfiller();
-  $('#bar3').barfiller();
-  $('#bar4').barfiller();
-  $('#bar5').barfiller();
-  $('#bar6').barfiller();
-
+  $("#bar1").barfiller();
+  $("#bar2").barfiller();
+  $("#bar3").barfiller();
+  $("#bar4").barfiller();
+  $("#bar5").barfiller();
+  $("#bar6").barfiller();
 })(jQuery);
